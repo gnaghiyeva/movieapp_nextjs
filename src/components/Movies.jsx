53040a31bd81ebc99c01d519293b4cd7b7358410
gnaghiyeva <gulnar.nagiyeva17@gmail.com>
2024-08-08 23:02:42 +0400

    movie detail page

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,15 +1,16 @@
 "use client"
 import React from 'react'
+import { useRouter } from 'next/navigation'
 import Image from 'next/image'
 const Movies = ({ dt }) => {
-
+  const router= useRouter();
   console.log(dt, "dt")
   return (
-    <div className='min-w-[470px] relative imgContainer cursor-pointer'>
-      <Image width={470} height={300} src={`https://image.tmdb.org/t/p/original/${dt.backdropt_path || dt.poster_path}`} />
+    <div onClick={()=>router.push(`/movie/${dt?.id}`)} className='min-w-[470px] relative imgContainer cursor-pointer'>
+      <Image style={{objectFit:'cover'}} width={470} height={300} src={`https://image.tmdb.org/t/p/original/${dt.backdropt_path || dt.poster_path}`} />
       <div className='absolute  bottom-0 p-3 w-full h-full flex flex-col justify-end opacity-0 hover:opacity-100 transition-opacity'>
-        <div className='text-2xl font-bold'>{dt.title}</div>
-        <div>{dt.release_date} - {dt.vote_average}</div>
+        <div className='text-2xl font-bold'>{dt?.title}</div>
+        <div>{dt?.release_date} - {dt?.vote_average}</div>
 
       </div>
     </div>
